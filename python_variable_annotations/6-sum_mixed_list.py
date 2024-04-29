2e8cca925925c18e8417cbf8d71333bd29a7dba1
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-04-29 12:57:02 +0400

    Union type

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Document for module"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Function method"""
+    return sum(mxd_lst)
