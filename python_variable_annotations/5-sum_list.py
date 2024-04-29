a2622ff1a8e7a9339c92d5ca766d270794d1061f
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-04-29 12:50:23 +0400

    list type-annotated

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Document for module"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Document for function"""
+    sum = 0
+    for item in input_list:
+        sum += item
+    return sum
