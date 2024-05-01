65de07e53a0e081204e30c2fa69f2bc43bb4333e
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-05-01 07:41:41 +0400

    multiple coroutine

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module document"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Wait_n document"""
+    my_list = []
+    for _ in range(n):
+        all_delays = await wait_random(max_delay)
+        my_list.append(all_delays)
+    my_list.sort()
+    return my_list
