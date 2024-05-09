6912a625a175cdb38746622810b3902c98254a2d
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-05-09 14:56:11 +0400

    assertation fix

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -32,10 +32,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        try:
-            assert isinstance(page, int) and isinstance(page_size, int)
-            assert page > 0
-            start_index, end_index = index_range(page, page_size)
-            return self.dataset()[start_index:end_index]
-        except TypeError:
-            print('type error')
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        return self.dataset()[start_index:end_index]
