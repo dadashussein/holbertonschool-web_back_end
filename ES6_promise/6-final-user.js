6885ec7b6a1f4d704343ce652fd779ba1b6f8876
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-05-24 12:25:25 +0400

    promise settled fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,5 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(filename)];
-  return Promise.allSettled(promises);
+
+  return Promise.allSettled(promises).then((results) => {
+    console.log(results);
+  });
 }
