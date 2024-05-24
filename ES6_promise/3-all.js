8d9ab9077f68047bb8c912d6e3fe039531e1384a
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-05-24 09:49:33 +0400

    promise all method

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/prefer-default-export */
+import { createUser, uploadPhoto } from './utils';
+
+export function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(values[0].body, values[1].firstName, values[1].lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
+
+handleProfileSignup();
