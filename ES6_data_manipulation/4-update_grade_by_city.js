22920ca0a7f29a02d3ce2109295cdb2861d709b7
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-05-26 22:44:04 +0400

    task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,11 @@
-/* eslint-disable no-param-reassign */
-export default function updateStudentGradeByCity(objs, city, newGrade) {
-  const students = objs.filter((student) => student.city === city);
-  // eslint-disable-next-line array-callback-return
-  students.map((student) => {
-    student.grade = newGrade;
-  });
-  return students;
+export default function updateStudentGradeByCity(student, city, newGrades) {
+  return student
+    .filter((item) => item.location === city)
+    .map((item) => {
+      const grades = newGrades.filter(
+        (grade) => grade.studentId === item.id,
+      );
+      const grade = grades.length ? grades[0].grade : 'N/A';
+      return { ...item, grade };
+    });
 }
